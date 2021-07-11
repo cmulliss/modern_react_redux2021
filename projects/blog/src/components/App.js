5494f717e50e274df52e553d1ad6fbfced187aa4
cmulliss <cmulliss@gmail.com>
2021-07-11 17:37:41 +0100

    initial commit

---
+++ projects/blog/src/components/App.js
@@ -0,0 +1,20 @@
+import './App.css'
+
+import CommentDetail from './CommentDetail'
+
+import faker from 'faker'
+faker.seed(234567)
+
+const App = () => {
+  return (
+    <div className='ui container comments'>
+      <CommentDetail />
+    </div>
+  )
+}
+
+export default App
+
+// fakerjs for fake data for projects, images in this case
+// npm i faker
+// bitbucket
