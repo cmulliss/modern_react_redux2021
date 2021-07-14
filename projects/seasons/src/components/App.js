a9029e0a77dfbec252732b09fbc1048d62cbce01
cmulliss <cmulliss@gmail.com>
2021-07-14 17:09:51 +0100

    geolocation

--- projects/seasons/src/components/App.js
+++ projects/seasons/src/components/App.js
@@ -3,6 +3,12 @@ import React from 'react'
 import './App.css'
 
 const App = () => {
+  // requires 2 fn callbacks, to give position, the first arg is going to be a fn callback that gets called any time this get or fn successfully gets the users position. Referred to as the 'success callback' followed by 2nd arg, failure callback, if not able to determine users physical location.
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position),
+    (err) => console.log(err)
+  )
+
   return (
     <div className='App'>
       <h1>App</h1>
@@ -11,3 +17,13 @@ const App = () => {
 }
 
 export default App
+
+/*
+Geolocation API in browser
+get users current location
+
+lattitude will tell you which hemisphere
+
+
+
+*/
