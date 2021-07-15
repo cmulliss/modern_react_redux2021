2c71a5d297fd95dd370e7dd93963b3b389de3af8
cmulliss <cmulliss@gmail.com>
2021-07-15 17:28:59 +0100

    coords and errors

--- projects/seasons/src/components/App.js
+++ projects/seasons/src/components/App.js
@@ -1,19 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import './App.css'
 
-const App = () => {
-  // requires 2 fn callbacks, to give position, the first arg is going to be a fn callback that gets called any time this get or fn successfully gets the users position. Referred to as the 'success callback' followed by 2nd arg, failure callback, if not able to determine users physical location.
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
+// default state value to null for as yet undetermined number
+class App extends Component {
+  state = {
+    lat: null,
+    errorMessage: ''
+  }
+  // requires 2 fn callbacks
+  // will be called frequently so don't want it in render method
+  position = window.navigator.geolocation.getCurrentPosition(
+    (position) => {
+      this.setState({
+        lat: position.coords.latitude
+      })
+    },
+    (err) => {
+      this.setState({ errorMessage: err.message })
+    }
   )
+  componentDidMount() {
+    console.log('my component rendered to the screen, mounted')
+  }
+  componentDidUpdate() {
+    console.log('my component was just updated - it rerendered!')
+  }
 
-  return (
-    <div className='App'>
-      <h1>App</h1>
-    </div>
-  )
+  render() {
+    if (this.state.errrorMessage && !this.state.lat) {
+      return <h2>Error: {this.state.errorMessage}</h2>
+    }
+
+    if (this.state.lat && !this.state.errorMessage) {
+      return <h2> Latitude: {this.state.lat} </h2>
+    }
+
+    return <h2>Loading...</h2>
+  }
 }
 
 export default App
@@ -24,6 +48,9 @@ get users current location
 
 lattitude will tell you which hemisphere
 
+// requires 2 fn callbacks, to give position, the first arg is going to be a fn callback that gets called any time this get or fn successfully gets the users position. Referred to as the 'success callback' followed by 2nd arg, failure callback, if not able to determine users physical location.
+
+component needs to rerender itself with the new geolocation info, which we have had to wait for, so needs to be a class.
 
 
 */
