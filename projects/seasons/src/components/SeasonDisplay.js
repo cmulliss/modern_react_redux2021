cf6916449f564255ca530f94a621686aee3d1fd5
cmulliss <cmulliss@gmail.com>
2021-07-17 17:41:00 +0100

    seasons

--- projects/seasons/src/components/SeasonDisplay.js
+++ projects/seasons/src/components/SeasonDisplay.js
@@ -1,17 +1,38 @@
 import React from 'react'
 
-getSeason = () => {
-    
+const seasonConfig = {
+  Summer: {
+    text: "Let's hit the beach!",
+    iconName: 'sun'
+  },
+  Winter: {
+    text: "Brr, it's chilly",
+    iconName: 'snowflake'
+  }
+}
+
+// fn we call with the current latitude and month
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'Summer' : 'Winter'
+  } else {
+    return lat > 0 ? 'Winter' : 'Summer'
+  }
 }
 
 const SeasonDisplay = (props) => {
-  console.log(props.lat)
+  const season = getSeason(props.lat, new Date().getMonth())
+  const { text, iconName } = seasonConfig[season] // {text, iconName}
   return (
     <div>
-      <h1>Seasons</h1>
-      {props.lat}
+      <i className={`${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`${iconName} icon`} />
     </div>
   )
 }
 
 export default SeasonDisplay
+
+// lat > 0, northern hemisphere, lat < 0, southern hemisphere
+// divisive whether to put ternary expression into the jsx, or whether to put it in a separate constant called text, then call that.
